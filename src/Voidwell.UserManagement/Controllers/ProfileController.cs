f3f45c8b40f9ddd9faf08e4e3bf24ef12d554ad9
Jess Tremper <jess@voidwell.com>
2018-10-30 22:22:21 -0400

    Return roles with claims.

--- src/Voidwell.UserManagement/Controllers/ProfileController.cs
+++ src/Voidwell.UserManagement/Controllers/ProfileController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Voidwell.UserManagement.Services;
@@ -23,13 +22,21 @@ namespace Voidwell.UserManagement.Controllers
         public async Task<IEnumerable<Claim>> GetUserClaims(Guid userId)
         {
             var user = await _userService.GetUser(userId);
+            var roles = await _userService.GetRoles(userId);
 
-            return new[]
+            var claims = new List<Claim>
             {
                 new Claim(JwtClaimTypes.Subject, userId.ToString()),
                 new Claim(JwtClaimTypes.Name, user.UserName),
                 new Claim(JwtClaimTypes.Email, user.Email)
             };
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(JwtClaimTypes.Role, role));
+            }
+
+            return claims;
         }
     }
 }
